e28ee08f9376f34901b1056edb0a773e5d3e2a38
Ola Glal <olaglal97@hotmail.com>
2018-04-08 00:42:10 +0200

    add Markers to Map

--- js/app.js
+++ js/app.js
@@ -1,30 +1,100 @@
+'use strict';
+
+/* Global Varibales */
+var map, infowindow, marker;
+
+/* Locations of the map */
+var locations = [
+	{
+		locationname: 'Lahalebo Restaurants',
+		lat: 30.7884573,
+		lng: 30.9978712,
+    },
+	{
+		locationname: 'Martin Juice',
+		lat: 30.7964486,
+		lng: 31.0008008,
+    },
+	{
+		locationname: 'Desoky And Soda',
+		lat: 30.7937944,
+		lng: 31.0012675,
+    },
+	{
+		locationname: 'Stereo Restaurant And Cafe',
+		lat: 30.7934105,
+		lng: 31.0017993,
+    },
+	{
+		locationname: 'Spectra Restaurant & Cafe',
+		lat: 30.7947187,
+		lng: 30.9964809,
+    },
+];
+
+var viewModel = function () {
+
+	var currentInfoWindow = null; 
+	var x = this;
+	x.selectLocation = ko.observableArray([]);
+
+	/* Put Location Markers */
+	locations.forEach(function (loc) {
+		var marker = new google.maps.Marker({
+			locationname: loc.locationname,
+			position: {
+				lat: loc.lat,
+				lng: loc.lng
+			},
+			map: map
+		});
+
+		x.selectLocation.push(marker);
+
+		/* Put Location Infowindow */
+		var infowindow = new google.maps.InfoWindow({
+			content: '<p>Marker Location: ' + marker.locationname + '</p>'
+		});
+
+		google.maps.event.addListener(marker, 'click', function () {
+			if (currentInfoWindow != null) {
+				currentInfoWindow.close();
+			}
+			infowindow.open(map, marker);
+			currentInfoWindow = infowindow;
+		});
+
+	});
+
+};
+
+
+
+/* Ininalize the Map */
 function initMap() {
 
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {
 			lat: 30.7974773,
 			lng: 31.0005147
 		},
-		zoom: 13
+		zoom: 15
 	});
 
 
-	var infowindow = new google.maps.InfoWindow();
+	infowindow = new google.maps.InfoWindow();
 	ko.applyBindings(new viewModel());
 
-	/*var marker = new google.maps.Marker({
-		position:,
-		map: map,
-		title: 
-	});
-	
-	marker.addListener('click', function () {
-		infowindow.open(map, marker);
-	});
-	
-	*/
 }
 
-function HandleError() {
-	alert('Invalid Map');
-}
+/* Handle Error of Map Loading */
+var mapFail = function () {
+	let el = document.getElementById('map').innerHTML = `
+  <div class="row column">
+    <div class="large alert callout">
+      <h3>Whoops!</h3>
+      <p>It seems something has gone terribly wrong... :(</p>
+    </div>
+  </div>
+  `;
+};
