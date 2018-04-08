47ea66e9360eeb0d9d318011516d3d7eb841c79a
Ola Glal <olaglal97@hotmail.com>
2018-04-08 19:38:53 +0200

    add WikiMedia API

--- js/app.js
+++ js/app.js
@@ -6,29 +6,39 @@ var map, infowindow, marker;
 /* Locations of the map */
 var locations = [
 	{
-		locationname: 'Lahalebo Restaurants',
-		lat: 30.7884573,
-		lng: 30.9978712,
+		locationname: 'Khan el-Khalili',
+		lat: 30.0477,
+		lng: 31.26230,
+		wikiID: 'Khan_el-Khalili',
     },
 	{
-		locationname: 'Martin Juice',
-		lat: 30.7964486,
-		lng: 31.0008008,
+		locationname: 'Egyptian Museum',
+		lat: 30.047503,
+		lng: 31.233702,
+		wikiID: 'Egyptian_Museum',
     },
 	{
-		locationname: 'Desoky And Soda',
-		lat: 30.7937944,
-		lng: 31.0012675,
+		locationname: 'Al-Hakim Mosque',
+		lat: 30.0546,
+		lng: 31.2642,
+		wikiID: 'Al-Hakim_Mosque',
     },
 	{
-		locationname: 'Stereo Restaurant And Cafe',
-		lat: 30.7934105,
-		lng: 31.0017993,
+		locationname: 'Mosque of Muhammad Ali',
+		lat: 30.0287,
+		lng: 31.2599,
+		wikiID: 'Mosque_of_Muhammad_Ali',
     },
 	{
-		locationname: 'Spectra Restaurant & Cafe',
-		lat: 30.7947187,
-		lng: 30.9964809,
+		locationname: 'Tahrir Square',
+		lat: 30.0444,
+		lng: 31.2357,
+		wikiID: 'Tahrir_Square',
+    }, {
+		locationname: 'Cairo Tower',
+		lat: 30.045916,
+		lng: 31.224291,
+		wikiID: 'Cairo_Tower',
     },
 ];
 
@@ -47,6 +57,7 @@ var viewModel = function () {
 	locations.forEach(function (loc) {
 		var marker = new google.maps.Marker({
 			locationname: loc.locationname,
+			wikiID: loc.wikiID,
 			position: {
 				lat: loc.lat,
 				lng: loc.lng
@@ -57,23 +68,40 @@ var viewModel = function () {
 		/* Array of Location as Markers */
 		selectLocation.push(marker);
 
-		/* Put Location Infowindow */
-		var infowindow = new google.maps.InfoWindow({
-			content: '<p>Marker Location: ' + marker.locationname + '</p>'
+		/* Wikipedia API*/
+
+		var wikiSource = 'https://en.wikipedia.org/wiki/' + loc.wikiID;
+
+		var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + loc.wikiID;
+		$.ajax({
+			type: 'GET',
+			dataType: 'jsonp',
+			data: {},
+			url: wikiURL
+		}).done(function (response) {
+			var extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
+			infowindow.setContent('<div>' + '<h4>' + loc.locationname + '</h4>' + extract + '<br>(Source: ' + '<a href=' + wikiSource + '>Wikipedia)</a>' + '</div>');
+
+			//Set Content if failure of AJAX request
+		}).fail(function (jqXHR, textStatus, errorThrown) {
+			infowindow.setContent('<div>' + 'No Service/ Connection Detected (Please try again later)' + '</div>');
 		});
 
+		var infowindow = new google.maps.InfoWindow();
+
+		/* Put Location Infowindow Action Listner */
 		google.maps.event.addListener(marker, 'click', function () {
 			if (currentInfoWindow != null) {
 				currentInfoWindow.close();
 			}
 			infowindow.open(map, marker);
 			currentInfoWindow = infowindow;
-			
+
 			marker.setAnimation(google.maps.Animation.BOUNCE);
-                setTimeout(function() {
-                    marker.setAnimation(null);
-                }, 1200);
-			
+			setTimeout(function () {
+				marker.setAnimation(null);
+			}, 1200);
+
 		});
 
 		/* Add Locations as List */
@@ -87,31 +115,45 @@ var viewModel = function () {
 		listElement.setAttributeNode(att);
 
 	});
-	
+
 	/* Event Listener of click on location on the list */
-	$("#names li").click(function(){
+	$("#names li").click(function (loc) {
 		var currentLocation = selectLocation()[this.id];
-		//alert(selectLocation()[this.id].locationname);
-		
+
 		//Animate its marker
 		currentLocation.setAnimation(google.maps.Animation.BOUNCE);
-                setTimeout(function() {
-                    currentLocation.setAnimation(null);
-                }, 1000);
-		
-		//Show InfoWindow
-		var infowindow = new google.maps.InfoWindow({
-			content: '<p>Marker Location: ' + currentLocation.locationname + '</p>'
+		setTimeout(function () {
+			currentLocation.setAnimation(null);
+		}, 1000);
+
+		/* Show InfoWindown */
+		var wikiSource = 'https://en.wikipedia.org/wiki/' + currentLocation.wikiID;
+
+		var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + currentLocation.wikiID;
+		$.ajax({
+			type: 'GET',
+			dataType: 'jsonp',
+			data: {},
+			url: wikiURL
+		}).done(function (response) {
+			var extract = response.query.pages[Object.keys(response.query.pages)[0]].extract;
+			infowindow.setContent('<div>' + '<h4>' + currentLocation.locationname + '</h4>' + extract + '<br>(Source: ' + '<a href=' + wikiSource + '>Wikipedia)</a>' + '</div>');
+
+			//Set Content if failure of AJAX request
+		}).fail(function (jqXHR, textStatus, errorThrown) {
+			infowindow.setContent('<div>' + 'No Service/ Connection Detected (Please try again later)' + '</div>');
 		});
-		
+
+		var infowindow = new google.maps.InfoWindow();
+
 		if (currentInfoWindow != null) {
-				currentInfoWindow.close();
-			}
-				
+			currentInfoWindow.close();
+		}
+
 		currentInfoWindow = infowindow;
 
 		infowindow.open(map, currentLocation);
-		
+
 	});
 
 
@@ -170,9 +212,7 @@ var viewModel = function () {
 			x.filteredLocation(filterInput);
 		}
 		infowindow.close();
-
 	};
-
 };
 
 /* Ininalize the Map */
@@ -180,12 +220,11 @@ function initMap() {
 
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {
-			lat: 30.7974773,
-			lng: 31.0005147
+			lat: 30.046345,
+			lng: 31.24171
 		},
-		zoom: 15
+		zoom: 14
 	});
-
 
 	infowindow = new google.maps.InfoWindow();
 	ko.applyBindings(new viewModel());
